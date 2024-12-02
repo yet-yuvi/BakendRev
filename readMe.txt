1965c6b4ca698483870d9469b46d02d132fcb706
yet-yuvi <oscyanogen@gmail.com>
2024-12-02 21:56:18 +0600

    Create express app, Create .gitignore file, Start the surver with specific port

---
+++ readMe.txt
@@ -0,0 +1,11 @@
+Commands:
+npm init
+npm i express --save
+npm i nodemon --save-dev
+npm i body-parser --save
+
+
+Git:
+git init
+git clone "link"
+.gitignore
---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+// Start the Server
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`)
+})
