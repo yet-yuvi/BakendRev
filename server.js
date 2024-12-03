7a1076fad103739f345a897951060627b97f072f
yet-yuvi <oscyanogen@gmail.com>
2024-12-04 01:45:23 +0600

    Define a simple route

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+// Define a simple route
+app.get('/', (req, res) => {
+    res.json({massage: 'WElcome to my application'})
+})
+
 // Start the Server
 const port = 3000;
 app.listen(port, () => {
