8c7a42cb692395525a2a89ef8145345ae3278e52
yet-yuvi <oscyanogen@gmail.com>
2024-12-05 04:48:23 +0600

    Get one user by valid id

--- server.js
+++ server.js
@@ -26,6 +26,21 @@ app.get('/users', (req, res) => {
     res.json(users);
 })
 
+// Retrive one user
+app.get('/users/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);    // Convert id to number, 10 specifies radix
+    if (isNaN(id)) {
+        return res.status(400).json({ message: "Invalid ID." }); // Validation Not-a-Number
+    }
+    const user = users.find((u) => u.id === id);  // Use strict equality
+    if(user) {
+        res.json(user);
+    }
+    else {
+        res.status(404).json({message: "User not found."});
+    }
+})
+
 // Start the Server
 const port = 3000;
 app.listen(port, () => {
