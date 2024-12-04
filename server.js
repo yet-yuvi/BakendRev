cf2830ea5038de65beaa1153365b67461fddb638
yet-yuvi <oscyanogen@gmail.com>
2024-12-05 02:45:30 +0600

    Create a user with post request and parse body_json with body-parser

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+// Parse request from body JSON
+app.use(bodyParser.json());
 
 // Define a simple route
 app.get('/', (req, res) => {
-    res.json({massage: 'WElcome to my application'})
+    res.json({massage: 'WElcome to my application'});
+})
+
+// Create a user
+app.post('/users', (req, res) => {
+    const user = req.body;
+    res.json(user);
 })
 
 // Start the Server
 const port = 3000;
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`)
+    console.log(`Server is running on port: ${port}`);
 })
