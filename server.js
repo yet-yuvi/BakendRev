04f1a8c30fb1b689d5e4c49678a2586daadad1ce
yet-yuvi <oscyanogen@gmail.com>
2024-12-05 03:26:34 +0600

    Create many user with each different ID

--- server.js
+++ server.js
@@ -10,9 +10,14 @@ app.get('/', (req, res) => {
     res.json({massage: 'WElcome to my application'});
 })
 
+let users = [];
+let lastId = 0;
+
 // Create a user
 app.post('/users', (req, res) => {
     const user = req.body;
+    user.id = ++lastId;
+    users.push(user);
     res.json(user);
 })
 
