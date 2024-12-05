b98e98229338602ae37bb8ffecd03bdd58fc9a7a
Jubayar Ahamad <oscyanogen@gmail.com>
2024-12-05 06:22:23 +0600

    Dev (#2)
    * Create a file
    
    * Create node app with npm init
    
    * Create express app, Create .gitignore file, Start the surver with specific port
    
    * Define a simple route
    
    * Install nodemon and start server.js file with nodemon
    
    * Create a user with post request and parse body_json with body-parser
    
    * Create many user with each different ID
    
    * Get all users
    
    * Get one user by valid id

--- server.js
+++ server.js
@@ -1,8 +1,48 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+// Parse request from body JSON
+app.use(bodyParser.json());
+
+// Define a simple route
+app.get('/', (req, res) => {
+    res.json({massage: 'WElcome to my application'});
+})
+
+let users = [];
+let lastId = 0;
+
+// Create a user
+app.post('/users', (req, res) => {
+    const user = req.body;
+    user.id = ++lastId;
+    users.push(user);
+    res.json(user);
+})
+
+// Retrive all users
+app.get('/users', (req, res) => {
+    res.json(users);
+})
+
+// Retrive one user
+app.get('/users/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);    // Convert id to number, 10 specifies radix
+    if (isNaN(id)) {
+        return res.status(400).json({ message: "Invalid ID." }); // Validation Not-a-Number
+    }
+    const user = users.find((u) => u.id === id);  // Use strict equality
+    if(user) {
+        res.json(user);
+    }
+    else {
+        res.status(404).json({message: "User not found."});
+    }
+})
 
 // Start the Server
 const port = 3000;
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`)
+    console.log(`Server is running on port: ${port}`);
 })
